88b62cb09af3c1d52cde91ed671914fa1ab90891
yuetsin <akaza_akari@sjtu.edu.cn>
2019-11-23 14:52:01 +0800

    [add] blind programming

--- SJTU-AI-Circuit-Design-Contest-2019/utils/converter/Rvoc_to_coco.py
+++ SJTU-AI-Circuit-Design-Contest-2019/utils/converter/Rvoc_to_coco.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
 
 import os
+import cv2
 import sys
 import PIL
 import json
 import random
 import train_list
+from pycocotools import mask as cocomask
 
 stuff_buf = train_list.stuff
 
-categories = [
-    "Aeroplane",
-    "Car",
-    "Sofa",
-    "TV Monitor"
-]
 
 total_count = sum([len(i) for i in stuff_buf])
 
@@ -22,8 +18,34 @@ id_pool = list(range(total_count))
 
 random.shuffle(id_pool)
 
-
 assert(len(stuff_buf) == 4)
+
+
+def __get_annotation__(self, mask, image=None):
+
+    _, contours, _ = cv2.findContours(
+        mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    segmentation = []
+    for contour in contours:
+        # Valid polygons have >= 6 coordinates (3 points)
+        if contour.size >= 6:
+            segmentation.append(contour.flatten().tolist())
+    RLEs = cocomask.frPyObjects(segmentation, mask.shape[0], mask.shape[1])
+    RLE = cocomask.merge(RLEs)
+    # RLE = cocomask.encode(np.asfortranarray(mask))
+    area = cocomask.area(RLE)
+    [x, y, w, h] = cv2.boundingRect(mask)
+
+    if image is not None:
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        cv2.drawContours(image, contours, -1, (0, 255, 0), 1)
+        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.imshow("", image)
+        cv2.waitKey(1)
+
+    return segmentation, [x, y, w, h], area
+
 
 coco_instance = {}
 
@@ -37,8 +59,23 @@ coco_instance["info"] = {"year": 2019,
 coco_instance["licenses"] = [{"id": 1,
                               "name": "copyright (c) 2019 contributors all rights reserved",
                               "url": "N/A"
+                              },
+                             {"id": 2,
+                              "name": "copyleft, under public domain.",
+                              "url": "N/A"
                               }]
 
+coco_instance["categories"] = [
+    {"supercategory": "Aeroplane", "id": 1, "name": "Aeroplane"},
+    {"supercategory": "Car", "id": 2, "name": "Car"},
+    {"supercategory": "Sofa", "id": 3, "name": "Sofa"},
+    {"supercategory": "TV Monitor", "id": 4, "name": "TV Monitor"}
+]
+
+counter = 0
+
+coco_instance["annotations"] = []
+coco_instance["images"] = []
 
 for index in range(4):
     for item in stuff_buf[index]:
@@ -49,3 +86,33 @@ for index in range(4):
 
         straint_box = item[1]
         assert(type(straint_box) == list and len(straint_box) == 4)
+
+        im = Image.open(file_path)
+        width, height = im.size
+
+        coco_instance["images"].append({
+            "license": 2,
+            "file_name": file_path,
+            "coco_url": "",
+            "height": height,
+            "width": width,
+            "date_captured": "",
+            "flickr_url": "",
+            "id": id_pool[counter]
+        })
+
+        img = im.convert('RGB')
+        mask = im.convert('L')
+
+        segmentation, bbox, area = self.__get_annotation__(mask, im)
+
+        coco_instance["annotations"].append({
+            "segmentation": segmentation,
+            "area": area,
+            "iscrowd": 0,
+            "image_id": id_pool[counter],
+            "bbox": [straint_box[0] * width, straint_box[1] * height, straint_box[2] * width, straint_box[3] * height],
+            "category_id": index + 1,
+            "id": counter
+        })
+        counter += 1
