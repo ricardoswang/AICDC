7a82189ef01b9a96a4097b5459457d2508f6a223
yuetsin <akaza_akari@sjtu.edu.cn>
2019-11-24 23:16:00 +0800

    add json files

---
+++ SJTU-AI-Circuit-Design-Contest-2019/utils/normalizer/normalizer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import json
+
+with open(input("Where's your result json file? >>>"), 'r') as f:
+    result_data = json.load(f)
+
+with open(input("Where's your origin json file? >>>"), 'r') as f:
+    origin_data = json.load(f)
+
+path_mapping = {}
+
+for img in origin_data["images"]:
+    path_mapping[img['id']] = (img['width'], img['height'])
+
+for result in result_data:
+
+
+{"image_id": 292,
+ "category_id": 0,
+    "bbox": [-4.66, -93.44, 10.72, 24.06],
+    "score": 0.01}
