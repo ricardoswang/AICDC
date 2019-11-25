66083ae56b1c557aa0c3d91b0a416ec9cbe9ad56
du <1299740655@qq.com>
2019-11-25 14:57:43 +0800

    delete image

--- run_bkp.sh
+++ run_bkp.sh
@@ -1,2 +1,2 @@
-python main.py ctdet --exp_id coco_dla --batch_size 32 --master_batch 1 --lr 1.25e-4  --gpus 0,1
+python main.py ctdet --exp_id coco_dla --batch_size 32 --master_batch 1 --lr 1.25e-4  --gpus 0
 
